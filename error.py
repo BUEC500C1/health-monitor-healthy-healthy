e2a18fc58e1f647e963de57ae61cd5186595d8ee
Chris Jung <cjung@bu.edu>
2020-03-21 02:27:36 -0400

    progress

--- error.py
+++ error.py
@@ -9,3 +9,46 @@ except KeyboardInterrupt:
     print("System Failed, reboot")
     global_kill.set()
     break
+    
+    
+    
+    def critical_blood_oxy(sig, frame):
+    while (True):
+    try:
+        print("Blood Oxygen is at a critical point")
+
+    except KeyboardInterrupt:
+        print("System Failed, reboot")
+        global_kill.set()
+        break
+        
+        #call alarm
+
+
+
+    def bloodOxygen_module(rate, global_kill):
+        
+        while True:
+        try:
+            globals.blood_oxy = random.randint(95, 99)
+
+        except KeyboardInterrupt:
+            print("System Failed, reboot")
+            global_kill.set()
+            break
+          
+            sleep(1/rate)
+
+    def check_blood_oxy():
+    
+    while True:
+    try:
+        if(globals.blood_oxy <= 94):
+            signal.alarm(.05)
+
+    except KeyboardInterrupt:
+        print("System Failed, reboot")
+        global_kill.set()
+        break
+        
+
