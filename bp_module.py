9bd846690762fb43f71abfbb5d6e1675f8d24074
Jacob Davis <jacobdavis608@gmail.com>
2020-03-24 17:09:55 -0400

    Fixing lint errors on CB and cleaning up

--- bp_module.py
+++ bp_module.py
@@ -7,14 +7,13 @@ def modify_bp(rate, bp, global_kill):
         try:
             bp += random.uniform(-1,1)
         
-            except KeyboardInterrupt:
-                print("System Failed, reboot")
-                global_kill.is_set():
-                break
+        except KeyboardInterrupt:
+            print("System Failed, reboot")
+            break
 
             # Print statements for testing
             # print("\nBlood pressure is: " + str(bp))
-            sleep(1/rate)
+        sleep(1/rate)
         
         
         
