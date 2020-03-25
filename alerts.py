4f26f6a66a471afd610b77874a12a9b712cac366
justinfmorgan <justinfmorgan@gmail.com>
2020-03-24 17:24:03 -0700

    Got a rough version of email alerts working

--- alerts.py
+++ alerts.py
@@ -1,3 +1,8 @@
+# Import smtplib for the actual sending function
+import smtplib
+
+# Import the email modules we'll need
+from email.message import EmailMessage
 
 def poll_vitals(pulse, bp, bo):
     '''
@@ -7,3 +12,25 @@ def poll_vitals(pulse, bp, bo):
     return ""
     
 
+def sendemail(alertMsg, subject):
+    msg = EmailMessage()
+    msg.set_content(str(alertMsg))
+
+    from_addr = 'healthyhealthyhippos@gmail.com'
+    to_addr_list = ['justinfm@bu.edu']
+
+    msg['Subject'] = 'Important Health Monitor Alert Regarding: ' + str(subject)
+    msg['From'] = from_addr
+    msg['To'] = ','.join(to_addr_list)
+
+    login = "healthyhealthyhippos"
+    password = "thehealthiesthippos"
+    smtpserver='smtp.gmail.com:587'
+
+    server = smtplib.SMTP(smtpserver)
+    server.connect("smtp.gmail.com", 587)
+    server.ehlo()
+    server.starttls()
+    server.login(login,password)
+    problems = server.send_message(msg)
+    server.quit()
