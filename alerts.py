064ea552a15b7eee2a998ee82728d6f3566c5f4b
Chris Jung <cjung@bu.edu>
2020-03-28 22:08:41 -0400

    add error check to alerts.py

--- alerts.py
+++ alerts.py
@@ -5,62 +5,73 @@ import smtplib
 from email.message import EmailMessage
 
 def poll_vitals(pulse, bp, bo):
-    '''
-    Check the values of the vitals of the patient.
-    Return a string indicating the alert, else ""
-    '''
-    alert_string = ""
-    only_alert = True   #for formatting 
-    if (pulse > 120):
-        only_alert = False 
-        alert_string += "Pulse is abnormally high"
-    
-    if (pulse < 50):
-        if (only_alert):
-            alert_string += "Pulse is abnormally low"
+    try:
+        
+        '''
+        Check the values of the vitals of the patient.
+        Return a string indicating the alert, else ""
+        '''
+        alert_string = ""
+        only_alert = True   #for formatting
+        if (pulse > 120):
             only_alert = False
-        else:
-            alert_string += "; Pulse is abnormally low"
-    
-    if (bp[0] > 160 or bp[1] > 90):
-        if (only_alert):
-            alert_string += "Blood pressure is abnormally high"
-            only_alert = False
-        else: 
-            alert_string += "; Blood pressure is abnormally high"
+            alert_string += "Pulse is abnormally high"
+        
+        if (pulse < 50):
+            if (only_alert):
+                alert_string += "Pulse is abnormally low"
+                only_alert = False
+            else:
+                alert_string += "; Pulse is abnormally low"
+        
+        if (bp[0] > 160 or bp[1] > 90):
+            if (only_alert):
+                alert_string += "Blood pressure is abnormally high"
+                only_alert = False
+            else:
+                alert_string += "; Blood pressure is abnormally high"
 
-    if (bp[0] < 90 or bp[1] < 70):
-        if (only_alert):
-            alert_string += "Blood pressure is abnormally low"
-        else: 
-            alert_string += "; Blood pressure is abnormally low"
+        if (bp[0] < 90 or bp[1] < 70):
+            if (only_alert):
+                alert_string += "Blood pressure is abnormally low"
+            else:
+                alert_string += "; Blood pressure is abnormally low"
 
-    if (bo < 94):
-        if (only_alert):
-            alert_string += "Blood oxygen is abnormally low"
-        else:
-            alert_string += "; Blood oxygen is abnormally low"
-    return alert_string
+        if (bo < 94):
+            if (only_alert):
+                alert_string += "Blood oxygen is abnormally low"
+            else:
+                alert_string += "; Blood oxygen is abnormally low"
+        return alert_string
+    except KeyboardInterrupt:
+        print("System Failed, reboot")
+        patient.end_vitals()
+        return
 
 def sendemail(alertMsg, subject):
-    msg = EmailMessage()
-    msg.set_content(str(alertMsg))
+    try:
+        msg = EmailMessage()
+        msg.set_content(str(alertMsg))
 
-    from_addr = 'healthyhealthyhippos@gmail.com'
-    to_addr_list = ['justinfm@bu.edu']
+        from_addr = 'healthyhealthyhippos@gmail.com'
+        to_addr_list = ['justinfm@bu.edu']
 
-    msg['Subject'] = 'Important Health Monitor Alert Regarding: ' + str(subject)
-    msg['From'] = from_addr
-    msg['To'] = ','.join(to_addr_list)
+        msg['Subject'] = 'Important Health Monitor Alert Regarding: ' + str(subject)
+        msg['From'] = from_addr
+        msg['To'] = ','.join(to_addr_list)
 
-    login = "healthyhealthyhippos"
-    password = "thehealthiesthippos"
-    smtpserver='smtp.gmail.com:587'
+        login = "healthyhealthyhippos"
+        password = "thehealthiesthippos"
+        smtpserver='smtp.gmail.com:587'
 
-    server = smtplib.SMTP(smtpserver)
-    server.connect("smtp.gmail.com", 587)
-    server.ehlo()
-    server.starttls()
-    server.login(login,password)
-    problems = server.send_message(msg)
-    server.quit()
+        server = smtplib.SMTP(smtpserver)
+        server.connect("smtp.gmail.com", 587)
+        server.ehlo()
+        server.starttls()
+        server.login(login,password)
+        problems = server.send_message(msg)
+        server.quit()
+    except:
+        print("System Failed, reboot")
+        patient.end_vitals()
+        return
